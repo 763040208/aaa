d86fe9a48ec9be354501c9e86308d4286721d6d0
wanghongfei <jiangyong763040208@qq.com>
2018-07-12 15:37:57 +0800

    我自己的分支

--- spring-quartz/src/main/java/com/quartz/demo/Main.java
+++ spring-quartz/src/main/java/com/quartz/demo/Main.java
@@ -2,10 +2,6 @@ package com.quartz.demo;
 
 public class Main {
 	public static void main(String[] args) {
-        System.out.println("学习学习");
-
-        System.out.println("好吧");
-
-        System.out.println("努力学习GIT");
+        System.out.println("我自己的分支");
     }
 }
