5bde6337797ce413a21a7bac1b2a68359a5c0f55
wanghongfei <jiangyong763040208@qq.com>
2018-07-12 15:43:25 +0800

    新添加了a方法

--- spring-quartz/src/main/java/com/quartz/demo/Main.java
+++ spring-quartz/src/main/java/com/quartz/demo/Main.java
@@ -3,5 +3,12 @@ package com.quartz.demo;
 public class Main {
 	public static void main(String[] args) {
         System.out.println("我自己的分支");
+        String a = a();
+        System.out.println(a);
     }
+
+    public static String a(){
+	    return "aaa";
+    }
+
 }
