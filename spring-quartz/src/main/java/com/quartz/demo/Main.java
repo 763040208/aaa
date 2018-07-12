a9a111771e727575f85aae34f26e02656adf36fa
wanghongfei <jiangyong763040208@qq.com>
2018-07-12 15:32:52 +0800

    master主分支

--- spring-quartz/src/main/java/com/quartz/demo/Main.java
+++ spring-quartz/src/main/java/com/quartz/demo/Main.java
@@ -2,9 +2,6 @@ package com.quartz.demo;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("你现在已经学会GIT提交了");
-        System.out.println("666的年轻人");
-        System.out.println("这是测试最后一遍");
-        System.out.println("小确丧");
+        System.out.println("这是master分支");
     }
 }
