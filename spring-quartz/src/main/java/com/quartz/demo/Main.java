9fb4f46e79bed5d18ed862c3c85ae51fdfc7cc75
wanghongfei <jiangyong763040208@qq.com>
2018-07-12 15:28:25 +0800

    好吧

--- spring-quartz/src/main/java/com/quartz/demo/Main.java
+++ spring-quartz/src/main/java/com/quartz/demo/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
         System.out.println("学习学习");
 
+        System.out.println("好吧");
+
         System.out.println("努力学习GIT");
     }
 }
